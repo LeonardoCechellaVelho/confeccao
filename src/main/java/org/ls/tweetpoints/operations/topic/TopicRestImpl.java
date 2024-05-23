acf2ad385571f75415cd288891c0f9c33b8a3c93
leocechellav@gmail.com <Leonardo>
2024-05-22 22:36:24 -0300

    Alterar endpoints de user para topic

--- src/main/java/org/ls/tweetpoints/operations/topic/TopicRestImpl.java
+++ src/main/java/org/ls/tweetpoints/operations/topic/TopicRestImpl.java
@@ -9,22 +9,22 @@ import jakarta.ws.rs.Path;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-@Path("/user")
+@Path("/topic")
 public class TopicRestImpl implements TopicRest {
     
-    TopicService userService;
+    TopicService topicService;
 
     @Override
     @Transactional
     public TopicResponse addTopic(TopicModel request) {
-        Topic topic = userService.addTopic(request);
+        Topic topic = topicService.addTopic(request);
         return TopicResponse.builder().topic(topic).build();
     }
 
     @Override
     @Transactional
     public TopicResponse getTopic(String name) {
-        Topic topic = userService.getTopic(new TopicModel(name));
+        Topic topic = topicService.getTopic(new TopicModel(name));
         return TopicResponse.builder().topic(topic).build();
     }
 }
